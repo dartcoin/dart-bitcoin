7a102801183d11a9448e4393e29181d1f5e1eeb3
Steven Roose <stevenroose@gmail.com>
2013-12-09 18:21:47 +0100

    serialisation example using ByteRepresantation

--- lib/src/core/varint.dart
+++ lib/src/core/varint.dart
@@ -1,25 +1,51 @@
 part of dartcoin;
 
-class VarInt {
+class VarInt extends Object with ByteRepresentation {
   
-  int value;
+  int _value;
   
-  VarInt(int this.value) {
-    if(value < 0) throw new Exception("VarInt values should be at elast 0!");
+  VarInt(int this._value) {
+    if(_value < 0) throw new Exception("VarInt values should be at elast 0!");
+  }
+  
+  factory VarInt.decode(Uint8List bytes) => _fromBytes(bytes);
+  
+  int get value {
+    _needInstance();
+    return _value;
   }
   
   int get size {
-    return sizeOf(this.value);
+    _needInstance();
+    return sizeOf(this._value);
   }
   
-  Uint8List encode() {
+  void _decode(Uint8List bytes) {
+    if(bytes.length < 1 || bytes.length > 9)
+      throw new Exception("VarInt are at least 1 and at most 9 bytes.");
+    if(bytes[0] == 253) {
+      _value = Utils.bytesToIntLE(bytes.sublist(1, 3));
+      return;
+    }
+    if(bytes[0] == 254) {
+      _value = Utils.bytesToIntLE(bytes.sublist(1, 5));
+      return;
+    }
+    if(bytes[0] == 255) {
+      _value = Utils.bytesToIntLE(bytes.sublist(1, 9));
+      return;
+    }
+    _value = bytes[0];
+  }
+  
+  Uint8List _encode() {
     List<int> result;
-    if(value < 0xfd)        result =  [value];
-    if(value <= 0xffff)     result = [253, 0, 0];
-    if(value <= 0xffffffff) result = [254, 0, 0, 0, 0];
+    if(_value < 0xfd)        result =  [_value];
+    if(_value <= 0xffff)     result = [253, 0, 0];
+    if(_value <= 0xffffffff) result = [254, 0, 0, 0, 0];
     if(result == null)      result = [255, 0, 0, 0, 0, 0, 0, 0, 0];
     
-    result.replaceRange(1, result.length, Utils.intToBytesLE(value));
+    result.replaceRange(1, result.length, Utils.intToBytesLE(_value));
     // sublist is necessary due to doubtful implementation of replaceRange
     return new Uint8List.fromList(result.sublist(0, size));
   }
