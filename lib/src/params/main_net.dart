72fadddb98647a4b6fd9359b606efd280f6a4be4
Steven Roose <stevenroose@gmail.com>
2014-02-20 00:02:04 +0100

    Added genesis block to NetworkParameters

--- lib/src/params/main_net.dart
+++ lib/src/params/main_net.dart
@@ -2,9 +2,17 @@ part of dartcoin.core;
 
 class _MainNetParams extends NetworkParameters {
   
+  static Block _genesis;
+  
   const _MainNetParams() : super._(
       addressHeader: 0, 
       magicValue: 0xD9B4BEF9,
       id: "org.bitcoin.production",
       port: 8333);
+  
+  Block get genesisBlock {
+    if(_genesis == null)
+      _genesis = NetworkParameters._createGenesis(this); 
+    return _genesis;
+  }
 }
--- lib/src/params/network_parameters.dart
+++ lib/src/params/network_parameters.dart
@@ -31,4 +31,26 @@ abstract class NetworkParameters {
     int this.port
   });
   
+  Block get genesisBlock;
+  
+
+
+  static Block _createGenesis(NetworkParameters params) {
+    Block genesisBlock = new Block(params: params);
+    Transaction t = new Transaction(params: params);
+    // A script containing the difficulty bits and the following message:
+    //
+    //   "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
+    Uint8List bytes = Utils.hexToBytes
+        ("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
+    t.addInput(new TransactionInput(scriptSig: new Script(bytes), params: params));
+    Script pubKeyScript = new ScriptBuilder(true)
+      .data(Utils.hexToBytes("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f"))
+      .op(ScriptOpCodes.OP_CHECKSIG)
+      .build();
+    t.addOutput(new TransactionOutput(value: Units.toSatoshi(50), scriptPubKey: pubKeyScript, params: params));
+    genesisBlock.addTransaction(t, false);
+    return genesisBlock;
+  }
+  
 }
